31d652b76e629e812986cd3eaa53d70e4dae3ec1
unknown <email@example.com>
2020-10-31 00:48:02 +0545

    new-commit

--- src/pages/AgaHotel/AgaBrand.js
+++ src/pages/AgaHotel/AgaBrand.js
@@ -5,7 +5,7 @@ import WeProvide from "../../components/AgaHotel/WeProvide";
 import DiscoverMore from "../../components/AgaHotel/DiscoverMore";
 import BrandContact from "../../components/brand/BrandContact";
 import Experience from "../../components/AgaHotel/Experience";
-import Room from "../../components/BrandCommon/Room";P
+import Room from "../../components/BrandCommon/Room";
 
 export default function AgaBrand() {
   return (
